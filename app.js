69799b92eab300ef227416bb7b9fb289d72fb42e
Oladee <deemajor230600@gmail.com>
2024-01-02 16:26:22 +0100

    My first full stack job

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const axios = require("axios")
+const { sendFile } = require("express/lib/response");
+const client = require("@mailchimp/mailchimp_marketing");
+const app = express()
+
+client.setConfig({
+    apiKey: "f9b4c48492ecbc384e66dec3edf34115-us17",
+    server: "us17",
+})
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/signup.html")
+})
+app.post("/",(req,res)=>{
+    var firstname = req.body.firstname
+    var lastname = req.body.lastname
+    var email = req.body.email
+    const run = async ()=>{
+        const response = await client.lists.batchListMembers("b788e6252d",{
+            members:[
+                {
+                    email_address: email,
+                    status: "subscribed",
+                    merge_fields:{
+                        FNAME: firstname,
+                        LNAME: lastname,
+                    }
+                }
+
+            ]
+        })
+        if(response.errors[0]?.error_code){
+            res.sendFile(__dirname + "/failure.html")
+            // res.write(`<h1>${response.errors[0]?.error_code}</h1>`)
+        }
+        else{
+            res.sendFile(__dirname + "/success.html")
+        }
+    }
+    run()
+})
+
+app.post("/failure.html", (req, res)=>{
+    res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Server is running on port 3000")
+})
